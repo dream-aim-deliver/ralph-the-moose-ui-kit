765fc19d2eb5ed070b95c6e5f197f26cb03ce377
maany <imptodefeat@gmail.com>
2024-01-31 17:56:04 +0100

    component: label, cleanup styles #5

--- lib/components/label/Label.tsx
+++ lib/components/label/Label.tsx
@@ -1,52 +1,22 @@
 import { twMerge } from "tailwind-merge";
-import {
-  primary as primaryStyles,
-  secondary as secondaryStyles,
-} from "./Label.styles";
 
 export interface LabelProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
+  variant: "small" | "medium";
   label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
 }
 
 /**
  * This ia a label component
  */
-export const Label = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
-  label,
-  ...props
-}: LabelProps) => {
-  const mode = primary ? primaryStyles : secondaryStyles;
-  const classes = twMerge(mode);
-  return (
-    <button
-      type="button"
-      className={classes}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}: {size}
-    </button>
-  );
+export const Label = ({ variant, label }: LabelProps) => {
+  const classes = twMerge([
+    "relative",
+    "text-sm",
+    "leading-[14px]",
+    "font-label-small",
+    variant === "small" ? "text-text-secondary" : "text-text-primary",
+    "text-text-primary",
+    "text-left",
+  ]);
+  return <div className={classes}>{label}</div>;
 };
