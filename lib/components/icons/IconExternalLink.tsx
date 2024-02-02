950d81ae36c00854a3c35aad161c18dbc61b804d
maany <imptodefeat@gmail.com>
2024-02-02 11:10:42 +0100

    component: icons, externalLink #8

---
+++ lib/components/icons/IconExternalLink.tsx
@@ -0,0 +1,11 @@
+import icon from "@/assets/icons/external-link.svg";
+import { Icon } from "./Icon";
+export const IconExternalLink = () => {
+  return (
+    <Icon
+      src={icon}
+      alt="external link"
+      className="w-full relative h-6 overflow-hidden"
+    />
+  );
+};
---
+++ stories/icons/IconExternalLink.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { IconExternalLink } from "@/components/icons";
+
+const meta = {
+  title: "Components/Icons/ExternalLink",
+  component: IconExternalLink,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} as Meta<typeof IconExternalLink>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
