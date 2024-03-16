27f77d63527ece7391493940cac04d3542a4891f
maany <imptodefeat@gmail.com>
2024-03-16 17:37:27 +0100

    Component: Ralph Logo

---
+++ lib/components/ralph-logo/RalphLogo.tsx
@@ -0,0 +1,74 @@
+import logo from "@/assets/ralph-logo.svg";
+
+import { twMerge } from "tailwind-merge";
+export interface RalphLogoProps {
+  variant: "icon" | "full-vertical" | "full-horizontal";
+}
+
+export const RalphLogo = ({ variant }: RalphLogoProps) => {
+  const ralphIcon = (
+    <div className="w-full relative flex justify-center items-center">
+      <img
+        className={twMerge("max-w-full overflow-hidden object-contain")}
+        alt="ralph"
+        src={logo}
+      />
+    </div>
+  );
+  const ralphText = (
+    <div
+      className={
+        "object-fill text-2xl font-semibold font-gluten text-base-colors/neutral-900 dark:text-base-colors/neutral-50 text-center p-0"
+      }
+    >
+      <div className="leading-[1.1]">
+        <span className="tracking-[-0.02em]">R</span>
+        <span className="tracking-[-0.06em]">a</span>
+        <span className="tracking-[-0.17em]">l</span>
+        <span className="tracking-[-0.06em]">p</span>
+        <span className="tracking-[-0.04em]">h</span>
+      </div>
+    </div>
+  );
+  const footerText = (
+    <div
+      className={twMerge(
+        " relative text-xs font-semibold font-gluten",
+        "text-base-colors/neutral-900 dark:text-base-colors/neutral-50",
+        "text-left inline-flex justify-center items-center",
+        "space-x-2",
+        "leading-snug",
+      )}
+    >
+      <div className="tracking-widest leading-snug">
+        <span className="tracking-widest">T</span>
+        <span className="tracking-widest">H</span>
+        <span className="tracking-widest">E</span>
+      </div>
+      <div className="tracking-widest leading-snug">
+        <span>M</span>
+        <span>O</span>
+        <span>O</span>
+        <span>S</span>
+        <span>E</span>
+      </div>
+    </div>
+  );
+  return (
+    <div
+      className={twMerge(
+        "flex w-full relative object-contain",
+        variant === "full-vertical" && "flex-col",
+        variant === "full-horizontal" && "flex-row",
+      )}
+    >
+      <div className="items-center">{ralphIcon}</div>
+      {variant !== "icon" && (
+        <div className="flex flex-col justify-center items-center">
+          {ralphText}
+          {footerText}
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ stories/brand-assets/RalphLogo.stories.ts
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { RalphLogo, type RalphLogoProps } from "@/components/ralph-logo";
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Brand Assets/Ralph Logo",
+  component: RalphLogo,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    variant: {
+      control: "inline-radio",
+      options: ["icon", "full-vertical", "full-horizontal"],
+    },
+  },
+} satisfies Meta<typeof RalphLogo>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Icon: Story = {
+  args: {
+    variant: "icon",
+  },
+};
+
+export const FullVertical: Story = {
+  args: {
+    variant: "full-vertical",
+  },
+};
+
+export const FullHorizontal: Story = {
+  args: {
+    variant: "full-horizontal",
+  },
+};
