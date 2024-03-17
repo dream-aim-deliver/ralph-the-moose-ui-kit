8044c6943c5fd62d7bd50af7059e92da7e80e151
maany <imptodefeat@gmail.com>
2024-03-17 19:26:12 +0100

    Legacy Dropdown

--- stories/layouts/Dropdown.stories.tsx
+++ stories/layouts/Dropdown.stories.tsx
@@ -29,7 +29,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    expanded: true,
+    expanded: false,
     // trigger: <div>Dropdown</div>,
     // items: [<div>Item 1</div>],
   },
