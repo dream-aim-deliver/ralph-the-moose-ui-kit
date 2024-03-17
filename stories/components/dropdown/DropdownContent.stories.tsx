19104a5299623773256f4449d48b81ebd6389521
maany <imptodefeat@gmail.com>
2024-03-17 21:20:24 +0100

    Dropdown Content

--- stories/components/dropdown/DropdownContent.stories.tsx
+++ stories/components/dropdown/DropdownContent.stories.tsx
@@ -1,42 +1,45 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { DropdownTrigger } from "@/components/dropdown/DropdownTrigger";
+import { DropdownContent } from "@/components/dropdown/DropdownContent";
 // import { IconNetworkBase } from "../../../lib";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Components/Drop Me Down/DropdownTrigger",
-  component: DropdownTrigger,
+  title: "Components/Drop Down/DropdownContent",
+  component: DropdownContent,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  // tags: ["autodocs"],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   variant: {
-  //     control: "inline-radio",
-  //     options: ["small", "large"],
-  //   },
-  //   selectedOption: { control: "text", defaultValue: "Base" },
-  //   expanded: { control: "boolean", defaultValue: false },
-  //   // onClick: { action: "clicked" },
-  //   // disabled: { control: "boolean" },
+  //   argTypes: {
+  // variant: {
+  //   control: "inline-radio",
+  //   options: ["small", "large"],
   // },
-} satisfies Meta<typeof DropdownTrigger>;
+  // selectedOption: { control: "text", defaultValue: "Base" },
+  // expanded: { control: "boolean", defaultValue: false },
+  // onClick: { action: "clicked" },
+  // disabled: { control: "boolean" },
+  //   },
+} satisfies Meta<typeof DropdownContent>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const sampleItems = [<div>Item 1</div>, <div>Item 2</div>];
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Small: Story = {
   args: {
-    title: "Network",
-    expanded: false,
-    variant: "small",
-    selectedOption: "Base",
-    icon: <></>,
+    children: sampleItems,
+    selectedItem: "Item 1",
+    // title: "Network",
+    // expanded: false,
+    // variant: "small",
+    // selectedOption: "Base",
+    // icon: <IconNetworkBase />,
   },
 };
