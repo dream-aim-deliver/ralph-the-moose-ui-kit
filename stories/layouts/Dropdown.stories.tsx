6562ed6b730f61f7090c5c59998d0aa6901e4ec3
maany <imptodefeat@gmail.com>
2024-03-17 17:21:34 +0100

    Component: Dropdown Init

---
+++ stories/layouts/Dropdown.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Dropdown } from "@/components/dropdown";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Layouts/Drop Down",
+  component: Dropdown,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // variant: {
+    //   control: "inline-radio",
+    //   options: ["primary", "secondary"],
+    // },
+    // onClick: { action: "clicked" },
+    // disabled: { control: "boolean" },
+  },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    expanded: true,
+    // trigger: <div>Dropdown</div>,
+    // items: [<div>Item 1</div>],
+  },
+};
