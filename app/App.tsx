87ed5b0443fa0346ec10036ead03481c6cdb31f3
maany <imptodefeat@gmail.com>
2024-03-18 01:09:42 +0100

    upgrade packages

--- app/App.tsx
+++ app/App.tsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { DropdownTrigger, IconNetworkBase } from "../lib";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <DropdownTrigger
+        title="Dropdown Trigger"
+        variant="small"
+        expanded={false}
+        icon={<IconNetworkBase />}
+      />
       <div className="text-3xl font-bold underline">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
