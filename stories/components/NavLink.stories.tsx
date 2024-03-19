7dce9dade8e4f948cc30f8e4eea899eb7dddbbd7
maany <imptodefeat@gmail.com>
2024-03-19 11:17:07 +0100

    Component: Navlink

---
+++ stories/components/NavLink.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { NavLink } from "@/components/nav-link";
+import { IconForm } from "../../lib";
+
+const meta = {
+  title: "Components/NavLink",
+  component: NavLink,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: { control: "inline-radio", options: ["small", "medium"] },
+    label: { control: "text" },
+    url: { control: "text" },
+  },
+} satisfies Meta<typeof NavLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    variant: "small",
+    label: "navlink / small",
+    url: "https://www.google.com",
+    icon: <IconForm />,
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    variant: "medium",
+    label: "navlink / medium",
+    url: "https://www.google.com",
+  },
+};
